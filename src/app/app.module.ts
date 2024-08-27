cdbc9238553ce8ddd3457e760898a2a682ba4a48
hndsrkc <hande.sirikci@fackler-it.com>
2024-08-27 13:58:36 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // elements belong to this module
+    AppComponent, AboutComponent, NavbarComponent, FooterComponent, HomeComponent
   ],
-  imports: [
+  imports: [ // other modules needed by the components of this module
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [], // elements that this module wants to share
+  providers: [], // global services that are accesible in any part of the application
+  bootstrap: [AppComponent] // component(s) that are loaded when this module bootstrapped
 })
 export class AppModule { }
